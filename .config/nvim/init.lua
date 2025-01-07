d4f44c9c4965ddfd005dfc4c79069419f86c2d60
Timour Ezeev <Timour.Ezeev@me.com>
2025-01-06 23:01:40 -0500

    Added new Neovim configurations.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,7 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- Set integration with system clipboard
+vim.cmd("set clipboard+=unnamedplus")
+
+vim.cmd("colorscheme catppuccin-macchiato")
