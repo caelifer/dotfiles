907af053315e2663bef3cbcde835fd205faba08d
Timour Ezeev <Timour.Ezeev@me.com>
2025-01-08 19:26:07 -0500

    Created a template to easily add new key mappings.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Template
+-- vim.keymap.set("n", "", "", { desc = "" })
+
+-- Yank to the end of line
 vim.keymap.set("n", "Y", "y$", { desc = "Copy/Yank to the end of line" })
