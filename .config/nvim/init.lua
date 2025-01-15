7767048a9974bf14d1cb7aa34df63ef6a3a9d0a8
Timour Ezeev <Timour.Ezeev@me.com>
2025-01-15 00:34:03 -0500

    Removed linewidth guidelines and cleanup Mason modules

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,8 +13,8 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
--- Set line width guidelines
-vim.cmd("set textwidth=100")
--- first break on 100th column, second on 132nd
-vim.cmd("set colorcolumn=+0,+32")
--- vim.cmd("highlight ColorColumn ctermbg=darkgrey guibg=lightgrey")
+-- -- Set line width guidelines
+-- vim.cmd("set textwidth=100")
+-- -- first break on 100th column, second on 132nd
+-- vim.cmd("set colorcolumn=+0,+32")
+-- -- vim.cmd("highlight ColorColumn ctermbg=darkgrey guibg=lightgrey")
