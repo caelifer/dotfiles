f249cea17290c753ebc381ec1eb2994f28c45264
Timour Ezeev <Timour.Ezeev@me.com>
2025-01-22 04:43:31 -0500

    Continue to fix formatting errors

--- .config/nvim/after/ftplugin/go.lua
+++
@@ -1,4 +0,0 @@
-vim.opt_local.textwidth = 4
-vim.opt_local.shiftwidth = 4
-vim.opt_local.expandtab = false
-vim.opt_local.autoindent = true
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,3 +18,12 @@ vim.g.loaded_ruby_provider = 0
 -- -- first break on 100th column, second on 132nd
 -- vim.cmd("set colorcolumn=+0,+32")
 -- -- vim.cmd("highlight ColorColumn ctermbg=darkgrey guibg=lightgrey")
+
+-- Global overrides for Go
+
+vim.g.go_recommended_style = 0
+vim.cmd("set textwidth=132")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set noexpandtab")
